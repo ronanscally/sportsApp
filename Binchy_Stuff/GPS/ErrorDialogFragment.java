613ddf0133b3b3f30716513815ca2d680ff5eb2c
Mark <markpurcell92@hotmail.com>
2014-11-14 22:01:24 +0000

    In folder SAD are software architecture examples.

---
+++ Binchy_Stuff/GPS/ErrorDialogFragment.java
@@ -0,0 +1,19 @@
+// Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
